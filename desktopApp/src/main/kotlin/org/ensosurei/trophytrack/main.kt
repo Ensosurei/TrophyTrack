d2489326713bf61a1eab605f0932675fe546e01e
Mercedes Ramos <mercedesramosvega13@gmail.com>
2026-06-01 08:58:46 -0700

    initialize AppContainer in desktop main
    Integrated DatabaseBuilder and DataStoreFactory into the AppContainer and passed it to the App composable in the desktop entry point.

--- desktopApp/src/main/kotlin/org/ensosurei/trophytrack/main.kt
+++ desktopApp/src/main/kotlin/org/ensosurei/trophytrack/main.kt
@@ -2,12 +2,17 @@ package org.ensosurei.trophytrack
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.ensosurei.trophytrack.database.DatabaseBuilder
+import org.ensosurei.trophytrack.preferences.DataStoreFactory
 
 fun main() = application {
+    val dataBuiler = DatabaseBuilder()
+    val store = DataStoreFactory()
+    val appContainer = AppContainer(store,dataBuiler)
     Window(
         onCloseRequest = ::exitApplication,
         title = "TrophyTrack",
     ) {
-        App()
+        App(appContainer)
     }
 }
