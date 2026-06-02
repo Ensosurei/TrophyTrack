1e35ad2a540d6658222dcb1f47324b9380fd003c
Mercedes Ramos <mercedesramosvega13@gmail.com>
2026-06-02 10:13:12 -0700

    ui: define color palette in Color.kt

---
+++ shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package org.ensosurei.trophytrack.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val background = Color(0xFF0D0D0D)
+val surface = Color(0xFF1A1A2E)
+val purple = Color(0xFF7C3AED)
+val pink = Color(0xFFEC4899)
+val white = Color(0xFFF8FAFC)
+val gray = Color(0xFF94A3B8)
+val green = Color(0xFF10B981)
+val blue = Color(0xFF3B82F6)
