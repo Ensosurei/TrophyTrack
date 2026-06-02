7f2469051a1e2f0075fb586daabb6aceb096dab4
Mercedes Ramos <mercedesramosvega13@gmail.com>
2026-06-02 10:22:10 -0700

    creation of the TrophyTrackTheme

---
+++ shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/theme/Theme.kt
@@ -0,0 +1,22 @@
+package org.ensosurei.trophytrack.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+
+private val ColorScheme = darkColorScheme(
+    background = background,
+    surface = surface,
+    primary = purple,
+    secondary = pink,
+    onBackground = white,
+    onSurface = white,
+    onPrimary = white
+)
+
+@Composable
+fun TrophyTrackTheme(content: @Composable () -> Unit){
+    MaterialTheme(colorScheme = ColorScheme){
+        content()
+    }
+}
