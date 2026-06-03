01e38e29aecc3858fd1120fbc98404ae324f9d5e
Mercedes Ramos <mercedesramosvega13@gmail.com>
2026-06-03 14:49:36 -0700

    creation of the GamesGrid and GameCard components

---
+++ shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/components/GamesGrid.kt
@@ -0,0 +1,55 @@
+package org.ensosurei.trophytrack.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.ensosurei.trophytrack.database.GameEntity
+import org.ensosurei.trophytrack.ui.theme.surface
+import org.ensosurei.trophytrack.ui.theme.white
+
+@Composable
+fun GamesGrid(
+    modifier: Modifier = Modifier,
+    gameList : List<GameEntity>
+){
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(140.dp),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+
+        modifier = modifier.fillMaxSize()
+    ){
+        items(gameList){ game ->
+            GameCard(
+                gameTitle = game.title
+            )
+        }
+    }
+}
+
+@Composable
+fun GameCard(gameTitle : String){
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = surface
+        ),
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(
+            text = gameTitle,
+            color = white,
+            modifier = Modifier.padding((16.dp))
+        )
+    }
+}
