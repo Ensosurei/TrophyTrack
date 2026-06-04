3ea5e13134d3710e38bbaf7b71a1eb6f71f3f336
Mercedes Ramos <mercedesramosvega13@gmail.com>
2026-06-04 00:49:37 -0700

    animate selection indicator and set minimum size for CategoryChip

--- shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/components/CategoryChip.kt
+++ shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/components/CategoryChip.kt
@@ -1,5 +1,6 @@
 package org.ensosurei.trophytrack.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -9,12 +10,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,9 +33,11 @@ fun CategoryChip(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    val alphaIndicator by animateFloatAsState(if(isSelected) 1f else 0f)
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
+            .sizeIn(minWidth = 48.dp, minHeight = 48.dp)
             .clickable(
                 interactionSource = remember { MutableInteractionSource() },
                 indication = null
@@ -49,14 +55,11 @@ fun CategoryChip(
 
         Spacer(modifier = Modifier.height(4.dp))
 
-        if(isSelected){
-            Box(
-                modifier = Modifier
-                    .size(6.dp)
-                    .background(pink, shape = CircleShape)
-            )
-        }else{
-            Spacer(modifier = Modifier.height(6.dp))
-        }
+        Box(
+            modifier = Modifier
+                .size(6.dp)
+                .background(pink, shape = CircleShape)
+                .graphicsLayer(alpha = alphaIndicator)
+        )
     }
 }
