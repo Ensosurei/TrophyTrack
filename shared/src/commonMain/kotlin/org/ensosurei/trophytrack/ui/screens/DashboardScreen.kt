71b989037989d41a844d901ce9eff4f7b06696c8
Mercedes Ramos <mercedesramosvega13@gmail.com>
2026-06-04 09:50:42 -0700

    update BarNavigation with a custom bottom bar layout and integrated FAB

--- shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/screens/DashboardScreen.kt
+++ shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/screens/DashboardScreen.kt
@@ -48,7 +48,7 @@ fun DashboardScreen(
 ){
     var currentScreen by remember {mutableIntStateOf(0)}
     var selectedCategoryIndex by remember { mutableIntStateOf(0) }
-    val categories = listOf("Todos", "Steam", "PlayStation", "Xbox")
+    val categories = listOf("All", "Steam", "PlayStation", "Xbox")
     val currentPlatform = categories[selectedCategoryIndex]
     val playingGames by container.db.gameDao().getPlayingGames().collectAsState(initial = emptyList())
     val completedGames by container.db.gameDao().getCompletedGames().collectAsState(initial = emptyList())
