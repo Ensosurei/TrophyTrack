4b6e7f4aaa9798e33b9a7782b8457672c1099cd1
Mercedes Ramos <mercedesramosvega13@gmail.com>
2026-06-04 02:40:41 -0700

    refactor App to use DashboardScreen and TrophyTrackTheme

--- shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/components/GameMiniCard.kt
+++ shared/src/commonMain/kotlin/org/ensosurei/trophytrack/ui/components/GameMiniCard.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import org.ensosurei.trophytrack.database.GameEntity
 import org.ensosurei.trophytrack.ui.theme.green
