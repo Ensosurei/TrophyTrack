d8a678695925d4330363f49c11b39c25858f5de4
Mercedes Ramos <mercedesramosvega13@gmail.com>
2026-06-05 18:47:39 -0700

    creation of the RawgModels data classes

---
+++ shared/src/commonMain/kotlin/org/ensosurei/trophytrack/network/RawgModels.kt
@@ -0,0 +1,26 @@
+package org.ensosurei.trophytrack.network
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RawgGameDto(
+    val name: String,
+    val background_image: String?,
+    val platforms: List<PlatformContainerDto>?
+)
+
+@Serializable
+data class GameSearchResponse(
+    val results : List<RawgGameDto>
+)
+
+@Serializable
+data class PlatformContainerDto(
+    val platform: RawgPlatformDto
+)
+@Serializable
+data class RawgPlatformDto(
+    val name: String
+)
+
+
